deda0a7a33cd981483f6d0783dcaefddbeeb3d13
aryagupta6 <aryaayra.g@gmail.com>
2026-04-23 23:44:44 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, request, render_template
+
+import pickle
+import numpy as np
+
+app = Flask(__name__)
+
+# Load mode1
+model = pickle.load(open("model.pkl", "rb"))
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    features = [float(x) for x in request.form.values()]
+    final_features = np.array([features])
+    
+    prediction = model.predict(final_features)
+
+    result = "Low Risk" if prediction[0] == 0 else "High Risk"
+
+    return render_template("index.html", prediction_text=result)
+
+
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
